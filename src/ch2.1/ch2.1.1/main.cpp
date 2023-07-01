e4c6dc4bb6c47a3d43fafc075eda7e5cbe3180e2
Sumant Kalra <57260000+sumant-kalra@users.noreply.github.com>
2023-07-01 10:59:57 +0530

    chg: dev: Problem 1 solved

--- src/ch2.1/ch2.1.1/main.cpp
+++ src/ch2.1/ch2.1.1/main.cpp
@@ -5,19 +5,18 @@
 
 #include <iostream>
 #include <string>
-#include <vector>
 #include <cmath>
-#include <sstream>
 #include <fstream>
 
 int main(int argc, char *argv[])
 {
     //[0] Inputs
-    double ox, oy, R, w, t0, tf, dt;
+
+    double cx, cy, R, w, t0, tf, dt;
     std::string garbageBuffer;
     std::cout << "# Provide the following inputs:\n";
-    std::cout << "Ox: CentreX(m) Oy: CentreY(m) R: Radius(m)\n";
-    std::cin >> ox >> oy >> R;
+    std::cout << "cx: CentreX(m) cy: CentreY(m) R: Radius(m)\n";
+    std::cin >> cx >> cy >> R;
     std::getline(std::cin, garbageBuffer);
     std::cout << "w: AngularVelocity(radians/s)\n";
     std::cin >> w;
@@ -25,34 +24,19 @@ int main(int argc, char *argv[])
     std::cout << "t0: StartTime(s) tf: EndTime(s) dt: OutputInterval(s)\n";
     std::cin >> t0 >> tf >> dt;
     std::getline(std::cin, garbageBuffer);
-
+    /// Print the inputs to the console for verification
     std::cout << "\n# The inputs provided are the following:\n";
     std::cout.flags(std::ios_base::fixed);
     std::cout.precision(3);
-    std::cout << "ox: " << ox << '\n';
-    std::cout << "oy: " << oy << '\n';
+    std::cout << "cx: " << cx << '\n';
+    std::cout << "cy: " << cy << '\n';
     std::cout << "R: " << R << '\n';
     std::cout << "w: " << w << '\n';
     std::cout << "t0: " << t0 << '\n';
     std::cout << "tf: " << tf << '\n';
     std::cout << "dt: " << dt << '\n';
 
-    //[1] Compute
-    int size = (tf - t0) / dt + 1;
-    std::vector<double> time;
-    time.reserve(size);
-    std::vector<double> x_d;
-    x_d.reserve(size);
-    std::vector<double> y_d;
-    y_d.reserve(size);
-    std::vector<double> vx_d;
-    vx_d.reserve(size);
-    std::vector<double> vy_d;
-    vy_d.reserve(size);
-    std::vector<double> ax_d;
-    ax_d.reserve(size);
-    std::vector<double> ay_d;
-    ay_d.reserve(size);
+    //[1] Compute and output to a text file
 
     double t = t0;
     double x = 0.0;
@@ -62,56 +46,26 @@ int main(int argc, char *argv[])
     double ax = 0.0;
     double ay = 0.0;
 
-    std::stringstream ss;
-
-    while (t <= tf)
-    {
-        time.emplace_back(t);
-
-        x = ox + R * cos(w * (t - t0));
-        x_d.emplace_back(x);
-
-        y = oy + R * sin(w * (t - t0));
-        y_d.emplace_back(y);
-
-        vx = -1 * w * R * sin(w * (t - t0));
-        vx_d.emplace_back(vx);
-
-        vy = w * R * cos(w * (t - t0));
-        vy_d.emplace_back(vy);
-
-        ax = -1 * w * w * R * cos(w * (t - t0));
-        ax_d.emplace_back(ax);
-
-        ay = -1 * w * w * R * sin(w * (t - t0));
-        ay_d.emplace_back(ay);
-
-        ss << t << ' ' << x << ' ' << y << ' ' << vx << ' ' << vy << ' ' << ax << ' ' << ay << '\n';
-
-        t += dt;
-    }
-
-    //[2] Output
-    // t x y vx vy
-    std::cout.flags(std::ios_base::fixed);
-    std::cout.precision(3);
-    std::cout << "\n#Outputs:\n";
-    std::cout << "t "
-              << "x "
-              << "y "
-              << "vx "
-              << "vy "
-              << "ax "
-              << "ay " << '\n';
-
-    for (int i = 0; i < time.size(); ++i)
-        std::cout << time.at(i) << " " << x_d.at(i) << " " << y_d.at(i) << " " << vx_d.at(i) << " " << vy_d.at(i) << " " << ax_d.at(i) << " " << ay_d.at(i) << '\n';
-
     std::ofstream outFile("Circle.out");
     if (!outFile.is_open())
         std::cerr << "Could not open the file for output!\n";
 
-    outFile << ss.str();
+    outFile.flags(std::ios_base::fixed);
+    outFile.precision(3);
+
+    while (t <= tf)
+    {
+        x = cx + R * cos(w * (t - t0));
+        y = cy + R * sin(w * (t - t0));
+        vx = -1 * w * R * sin(w * (t - t0));
+        vy = w * R * cos(w * (t - t0));
+        ax = -1 * w * w * R * cos(w * (t - t0));
+        ay = -1 * w * w * R * sin(w * (t - t0));
+        /// t x y vx vy ax ay
+        outFile << t << ' ' << x << ' ' << y << ' ' << vx << ' ' << vy << ' ' << ax << ' ' << ay << '\n';
+
+        t += dt;
+    }
 
     outFile.close();
 
